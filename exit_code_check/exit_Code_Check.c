4f36147c9cfaa7883eb53ccd1d566333774ab4c2
vishnucsn <vishnucsnpro@gmail.com>
2023-12-25 14:14:11 +0530

    Added a tab space`:

--- exit_code_check/exit_Code_Check.c
+++ exit_code_check/exit_Code_Check.c
@@ -5,7 +5,7 @@ int main(){
 
     printf("\n\n**** My exit code check functionality implementation ****\n\n");
     printf("Now this one got exited with error code (2) and you can check this\
- error code in the windows terminal when you give the below command \n");
+	error code in the windows terminal when you give the below command \n");
     printf("\n\techo %%errorlevel%% \n");
     exit(2);
 
