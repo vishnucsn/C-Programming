7af93afe236c5bf5777243f68d919d764c7e6fac
vishnucsn <vishnucsnpro@gmail.com>
2024-07-31 06:43:24 +0530

    Bit manipulation series 1

---
+++ Bit_manipulations/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+
+bool getBitValue(uint8_t bit_pos, uint32_t input)
+{
+    bool ret = 0;
+    bool bin_arr[32] ={0};
+    for(int i=0; i<=31; i++)
+    {
+        bin_arr[i] = (input % 2);
+        input = input/2;
+    }
+
+    for(int i=0;i<32;i++)
+    {
+        if(i == bit_pos)
+            {
+                ret = bin_arr[i];
+                break;
+            }
+        //printf("bin_arr[%d] - %d ",i,bin_arr[i]);
+    }
+
+    return ret;
+}
+
+
+/*Function which is used to swap the bit values present in the bit positions which are passed as argument*/
+uint32_t update_bit_positions(uint8_t x_pos, uint8_t y_pos, uint32_t input)
+{
+    bool temp =0,x_val=0,y_val=0;
+
+    printf("Inpt - %u\n",input);
+    x_val = getBitValue(x_pos,input);
+    y_val = getBitValue(y_pos,input);
+    printf("x_val - %d  y_val - %d\n",x_val,y_val);
+    temp = x_val;
+    x_val = y_val;
+    y_val = temp;
+    printf("x_val - %d  y_val - %d\n",x_val,y_val);
+
+    input &= ~( (1 << x_pos) | (1 << y_pos) ); //clear the bit positions(x_pos and y_pos)
+    input |= ( (x_val << x_pos) | (y_val << y_pos) ); //Set the bit positions(x_pos and y_pos) with the desired value(x_val and y_val)
+    printf("Inpt - %d\n",input);
+
+    return input;
+}
+
+int main()
+{
+    uint32_t input = 0x54,output = 0;
+    //bool val =0;
+
+    
+    
+    output = update_bit_positions(3,6,input);
+    printf("output = %d\n",output);
+
+
+    return 0;
+}
